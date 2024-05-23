86d6db4befdec631a673d3892044638e3be4653f
rkd0838 <rkdrnjs00@gmail.com>
2024-05-23 17:29:45 +0900

    3일차 수업 오후까지

--- src/main/java/com/study/java_study/ch04_제어/조건문01.java
+++ src/main/java/com/study/java_study/ch04_제어/조건문01.java
@@ -11,5 +11,25 @@ public class 조건문01 {
         } else {
             System.out.println("거짓입니다");
         }
+
+        int num = 10;
+        if(num % 2 == 0 && num % 10 == 0) {
+            System.out.println("짝수입니다");
+        } else {
+            System.out.println("홀수입니다");
+        }
+
+
+        if(num % 2 == 0) System.out.println("2의 배수입니다");
+
+        if(num % 2 ==0) {
+            System.out.println("2의 배수입니다");
+        } else if(num % 3 == 0) {
+            System.out.println("3의 배수입니다");
+        } else if(num % 7 == 0) {
+            System.out.println("7의 배수입니다");
+        } else {
+            System.out.println("2의 배수도 아니고 3의 배수도 아닙니다");
+        }
     }
 }
