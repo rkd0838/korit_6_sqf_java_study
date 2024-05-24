3a1a6ba6cf82de5b7ac5fc415b015abde78c722c
rkd0838 <rkdrnjs00@gmail.com>
2024-05-24 17:50:52 +0900

    4일차 오후수업 (수정 삭제 미완)

--- src/main/java/com/study/java_study/ch06_배열/ArrayService.java
+++ src/main/java/com/study/java_study/ch06_배열/ArrayService.java
@@ -37,65 +37,65 @@ public class ArrayService {
 
                     break;
                 case "2":
-//                    System.out.println("[수정]");
-//                    for (int i = 0; i < names.length; i++) {
-//                        newNames[i] = names[i];
-//                    }
-//                    names = newNames;
-//                    System.out.print("수정 할 이름 : ");
-//                    String result2 = null;
-//                    String findName2 = scanner.nextLine();
-//                    for (String name : names) {
-//                        if (name.equals(findName2)) {
-//                            result2 = name;
-//                            System.out.print("수정 할 이름을 입력하세요 : ");
-//                            String  = scanner.nextLine();
-//                            break;
-//                        }
-//                    }
-//                    if (result2 == null) {
-//                        System.out.println("해당 이름은 존재하지 않는 이름입니다.");
-//                        break;
-//                    }
-//
-//                    System.out.println("수정 완료!");
+                    System.out.println("[수정]");
+                    for (int i = 0; i < names.length; i++) {
+                        newNames[i] = names[i];
+                    }
+                    names = newNames;
+                    System.out.print("수정 할 이름 : ");
+                    String result2 = null;
+                    String findName2 = scanner.nextLine();
+                    for (String name : names) {
+                        if (name.equals(findName2)) {
+                            result2 = name;
+                            System.out.print("수정 할 이름을 입력하세요 : ");
+                            newNames  = scanner.nextLine();
+                            break;
+                        }
+                    }
+                    if (result2 == null) {
+                        System.out.println("해당 이름은 존재하지 않는 이름입니다.");
+                        break;
+                    }
+
+                    System.out.println("수정 완료!");
 
                     break;
                 case "3":
-                    System.out.println("[삭제]");
-                    System.out.println("삭제 할 이름 : ");
-                    String result = null;
-                    String deleteName = scanner.nextLine();
-                    for (String name : names) {
-                        if (name.equals(deleteName)) {
-                            result = name;
-                            break;
-                        }
-                    }
-                    if (result == null) {
-                        System.out.println("해당 이름은 존재하지 않는 이름입니다.");
-                        break;
-                    }
-                    System.out.println("이름이 삭제되었습니다.");
-                    names
-                    break;
-                case "4":
-                    System.out.println("[이름 찾기]");
-                    System.out.print("조회 할 이름 : ");
-                    String result = null;
-                    String fineName = scanner.nextLine();
-                    for (String name : names) {
-                        if (name.equals(fineName)) {
-                            result = name;
-                            break;
-                        }
-                    }
-                    if (result == null) {
-                        System.out.println("해당 이름은 존재하지 않는 이름입니다.");
-                        break;
-                    }
-                    System.out.println("해당 이름은 등록된 이름입니다.");
-                    break;
+//                    System.out.println("[삭제]");
+//                    System.out.println("삭제 할 이름 : ");
+//                    String result = null;
+//                    String deleteName = scanner.nextLine();
+//                    for (String name : names) {
+//                        if (name.equals(deleteName)) {
+//                            result = name;
+//                            break;
+//                        }
+//                    }
+//                    if (result == null) {
+//                        System.out.println("해당 이름은 존재하지 않는 이름입니다.");
+//                        break;
+//                    }
+//                    System.out.println("이름이 삭제되었습니다.");
+//                    names
+//                    break;
+//                case "4":
+//                    System.out.println("[이름 찾기]");
+//                    System.out.print("조회 할 이름 : ");
+//                    String result = null;
+//                    String fineName = scanner.nextLine();
+//                    for (String name : names) {
+//                        if (name.equals(fineName)) {
+//                            result = name;
+//                            break;
+//                        }
+//                    }
+//                    if (result == null) {
+//                        System.out.println("해당 이름은 존재하지 않는 이름입니다.");
+//                        break;
+//                    }
+//                    System.out.println("해당 이름은 등록된 이름입니다.");
+//                    break;
                 case "5":
                     System.out.println("[전체 조회]");
                     for (int i = 0; i < names.length; i++) {
